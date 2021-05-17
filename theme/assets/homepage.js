0bdcd6c14dfe60593531ab4e0991467c27042c22
Nguyen Dinh Phuc <dinhphuc208@gmail.com>
2021-05-17 17:59:33 +0700

    Update 17/5

--- theme/assets/homepage.js
+++ theme/assets/homepage.js
@@ -47,3 +47,16 @@ function navMenu() {
 }
 
 navMenu()
+
+
+// Popup Open
+function popupOpen() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+// Popup Close
+function popupClose() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
